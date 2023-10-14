f7d002d6735671002ac9b193a3a7ccb2998d12d4
Xavier2323 <98024928+Xavier2323@users.noreply.github.com>
2023-10-15 00:21:03 +0800

    Update ExtendGCD.cpp

--- codes/Math/ExtendGCD.cpp
+++ codes/Math/ExtendGCD.cpp
@@ -1,17 +1,11 @@
-// @return $x, y$ s.t. $ax + by = \gcd(a, b)$
-#define ll long long
+// ax + by = gcd(a, b)
 ll ext_gcd(ll a, ll b, ll& x, ll& y) {
 	if(b == 0) {
-		x = 1; y = 0;
+		x = 1, y = 0;
 		return a;
 	}
-	ll x2, y2;
-	ll c = a % b;
-	if(c < 0) c += b;
-	ll g = ext_gcd(b, c, x2, y2);
-	x = y2;
-	y = x2 - (a / b) * y2;
+	ll x1, y1;
+	ll g = ext_gcd(b, a % b, x1, y1);
+	x = y1, y = x1 - (a / b) * y1;
 	return g;
 }
-//a^{-1} % p = x % p
-
